038c56f4396ffd4d2f089c0f6bfa99dd1e459114
Rupeshh1 <rupesh6658@gmail.com>
2022-07-25 12:59:39 +0530

    third commit

---
+++ src/test/java/GitPractice.java
@@ -0,0 +1,14 @@
+
+
+public class GitPractice {
+	
+	public void practice1Test1()
+	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println("----->browser is"+BROWSER+"---->url is"+URL);
+	}
+
+}
+//change the status
+// i love my india
